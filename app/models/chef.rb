6b7f11e9af5ef104f7c12dcd9d87b2625bbf595f
Faiz-Muhammad <practice.rubyonrails1@gmail.com>
2019-01-31 18:51:23 +0500

    Implement chef signup

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,10 +1,9 @@
 class Chef < ApplicationRecord
   before_save { self.email = email.downcase }
-  validates_presence_of :chefname, length: {maximum: 30}
+  validates :chefname, presence: true, length: { maximum: 4 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates_presence_of :email, length: { maximum: 255},
-                        format: { with: VALID_EMAIL_REGEX },
-                        uniqueness: {case_sensitive: false}
-  has_many :recipes                       
-
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 5 }
+  has_many :recipes
 end
