d93f9be66906894d8a36c4e7b174fd6179ce4fb7
Faiz-Muhammad <practice.rubyonrails1@gmail.com>
2019-02-15 15:55:00 +0500

    complete real time comments on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.web_socket_server_url = "wss://dry-meadow-69861/cable"
+  config.action_cable.allowed_request_origins = ['https://dry-meadow-69861.herokuapp.com','http://dry-meadow-69861.herokuapp.com']
 end
